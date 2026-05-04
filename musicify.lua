a57374172c5d6208d5b1d24bd3014fc765ede33d
Highopes109 <jaa03092009@outlook.com>
2026-05-03 21:05:30 -0700

    Update musicify.lua

--- musicify.lua
+++ musicify.lua
@@ -80,15 +80,16 @@ local function play(songID)
     while true do
         local chunk = h.read(16 * 1024)
         if not chunk then break end
-        buffer = decoder(chunk)
-        for i,_ in pairs(buffer) do
-          buffer[i] = buffer[i] * softVolume
-          if buffer[i] > 127 then
-            buffer[i] = 127
-          elseif buffer[i] < -127 then
-            buffer[i] = -127
-          end
-        end
+       	local buffer = decoder(chunk)
+	local play_functions = {}
+	for _, speaker in pairs(speakers) do
+		table.insert(play_functions, function()
+    			while not speaker.playAudio(buffer) do
+        			os.pullEvent("speaker_audio_empty")
+    			end
+		end)
+	end
+    parallel.waitForAll(table.unpack(play_functions))
         if modem and serverMode then
           modem.transmit(serverChannel,serverChannel,buffer)
         end
